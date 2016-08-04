36057851de09bf8dc256c7cea96f4b1b036dde90
18chowdhary <gwc@Girls-Who-Code-27.local>
2016-08-04 13:26:29 -0400

    Master Project
    This will be our final product. Check the comment block at the top of
    GameScene.swift for more info

---
+++ Master Project - Roardash/Master Project - Roardash/GameScene.swift
@@ -0,0 +1,52 @@
+//
+//  GameScene.swift
+//  Master Project - Roardash
+/*
+ Check here for all the information about this project!
+ - About this project
+    This project holds all the files needed to run our app. This is what we will be showing as the results of our efforts. 
+ - Storyboarding & other set-up stuff 
+    The first thing I did was delete the existing Game View Controller because we don't want it right now. 
+ 
+ */
+//  Created by Girls Who Code on 8/4/16.
+//  Copyright (c) 2016 Girls Who Code. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
