09ab49e70ef8cf31a679cd364109df0c6e4fafdd
rowanmacy <rowanmacy@gmail.com>
2016-08-08 09:46:47 -0400

    "Roar" in GameViewController

---
+++ Sound/LionAnimation/GameViewController.swift
@@ -0,0 +1,61 @@
+//
+//  GameViewController.swift
+//  LionAnimation
+//
+//  Created by Girls Who Code on 8/4/16.
+//  Copyright (c) 2016 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import AVFoundation
+
+class GameViewController: UIViewController {
+
+    var myAudioPlayer = AVAudioPlayer()
+    
+//    @IBAction func callRoar(sender: AnyObject) {
+//        myAudioPlayer.play()
+//    }
+    
+    @IBAction func callRoar(sender: AnyObject) {
+        myAudioPlayer.play()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed:"GameScene") {
+            // Configure the view.
+//            let skView = self.view as! SKView
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+//            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+//            scene.scaleMode = .AspectFill
+            
+//            skView.presentScene(scene)
+        }
+        
+        let lionRoarPathString = NSBundle.mainBundle().pathForResource("lion_roar", ofType: "mp3")
+        
+        if let lionRoarPathString = lionRoarPathString {
+            let soundURL = NSURL(fileURLWithPath: lionRoarPathString)
+            do {
+                try myAudioPlayer = AVAudioPlayer(contentsOfURL: soundURL)
+            } catch {
+                print("Error with sound playback")
+            }
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
