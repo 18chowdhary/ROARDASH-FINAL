85bfe68df943b3efb4ad4277553a7f257d062b8f
rowanmacy <rowanmacy@gmail.com>
2016-08-08 09:12:25 -0400

    Welcome Screen
    Has a random message that greets the user every time it runs.

---
+++ Welcome Screen with messages/Welcome Screen with messages/WelcomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WelcomeViewController.swift
+//  Welcome Screen with messages
+//
+//  Created by Girls Who Code on 8/6/16.
+//  Copyright © 2016 Girls Who Code. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class WelcomeViewController : UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    let array = ["Hey you", "Hi", "Hello, world", "Strong looks good on you", "You're going to kick asphalt today", "Don't be afraid to fail, be afraid not to try", "You got this", "Be the little engine that did it"]
+
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.orangeColor()
+        
+        func random() -> Int {
+            return Int(Float(arc4random_uniform(6)))
+        }
+        
+        func displayMessage() {
+            messageLabel.text = array[random()]
+        }
+        displayMessage()
+
+    }
+    
+}
