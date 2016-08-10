72ad18ce9cee80310a546f630bc06b2cb19c9ee1
rowanmacy <rowanmacy@gmail.com>
2016-08-09 22:03:11 -0400

    More UI adjustments
    -Created another background to use in End Scene (pattern_2)
    -Added more constraints where needed
    -Grammar changes to motivational quotes
    -The launch screen’s logo still does not align with the Home Scene
    logo. I got it once but I don’t know what happened and now it won’t
    display without a slight niggling disparity
    -Added 20 new possible welcome messages

---
+++ Working launch screen/Running screen/GeneralViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralViewController.swift
+//  Running screen
+//
+//  Created by Girls Who Code on 8/9/16.
+//  Copyright © 2016 Girls Who Code. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EndViewController : UIViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor(patternImage:UIImage(named: "pattern_2.png")!)
+
+    }
+}
